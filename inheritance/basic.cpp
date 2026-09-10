f581b4c308adff2476f1bd87b2b0dd9aee8b10a2
TechAsish <gitasish1212@gmail.com>
2026-09-11 00:24:47 +0530

    feat:: inheritance with access modifier

--- inheritance/basic.cpp
+++ inheritance/basic.cpp
@@ -1,20 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Parent{
-    public:
-    void display(){
-        cout<<"This is Parent class"<<endl;
-    }
+class Human
+{
+protected:
+    string name;
+    int age, weight;
 };
 
-class Child: public Parent{
-    Parent obj;
+class Student : protected Human
+{
+private:
+    int roll_no, fees;
+
+public:
+    void setData(string n, int a, int w, int r, int f)
+    {
+        name = n;
+        age = a;
+        weight = w;
+        roll_no = r;
+        fees = f;
+    }
+
+    void displayData()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Weight: " << weight << endl;
+        cout << "Roll No: " << roll_no << endl;
+        cout << "Fees: " << fees << endl;
+    }
 };
 
 int main()
 {
-    Child obj;
-    obj.display();
+    Student obj;
+    obj.setData("Asish", 20, 60, 101, 5000);
+    obj.displayData();
     return 0;
 }
