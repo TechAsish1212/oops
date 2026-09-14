9029616288d4020b9fe7e354a3db89f618de5783
TechAsish <gitasish1212@gmail.com>
2026-09-15 01:12:20 +0530

    feat:: learn about method overriding

--- pillars/polymorphism/run-time/methodOverriding.cpp
+++ pillars/polymorphism/run-time/methodOverriding.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal Sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog Barks" << endl;
+    }
+};
+
+int main() {
+    Animal* ptr;
+    Dog d;
+
+    ptr = &d;
+    ptr->sound();
+
     return 0;
 }
